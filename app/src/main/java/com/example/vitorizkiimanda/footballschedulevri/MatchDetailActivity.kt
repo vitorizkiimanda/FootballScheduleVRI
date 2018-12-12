ff20a05c102678e4a21edeece47d812df4285293
Vito Rizki Imanda <vitorizkiimanda@gmail.com>
2018-12-12 12:35:03 +0700

    detail page : got data

--- app/src/main/java/com/example/vitorizkiimanda/footballschedulevri/MatchDetailActivity.kt
+++ app/src/main/java/com/example/vitorizkiimanda/footballschedulevri/MatchDetailActivity.kt
@@ -2,11 +2,17 @@ package com.example.vitorizkiimanda.footballschedulevri
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.vitorizkiimanda.footballschedulevri.Api.Data.Match
 
 class MatchDetailActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_match_detail)
+
+
+        val data:Match = intent.getParcelableExtra<Match>("match")
+        Log.d("detail page", "data detail :"+ data)
     }
 }
