d6f25f681dfa3f3af0005c7ae8f7b07c5a49e9ca
Vito Rizki Imanda <vitorizkiimanda@gmail.com>
2018-12-12 12:12:26 +0700

    first api

--- app/src/main/java/com/example/vitorizkiimanda/footballschedulevri/FirstFragment.kt
+++ app/src/main/java/com/example/vitorizkiimanda/footballschedulevri/FirstFragment.kt
@@ -4,6 +4,7 @@ package com.example.vitorizkiimanda.footballschedulevri
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v4.widget.SwipeRefreshLayout
+import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.util.Log
 import android.view.LayoutInflater
@@ -11,8 +12,16 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ProgressBar
 import android.widget.Spinner
+import com.example.vitorizkiimanda.footballschedulevri.Adapter.MatchAdapter
+import com.example.vitorizkiimanda.footballschedulevri.Api.ApiRepository
+import com.example.vitorizkiimanda.footballschedulevri.Api.Data.Match
+import com.example.vitorizkiimanda.footballschedulevri.Api.Data.Team
+import com.google.gson.Gson
 import kotlinx.android.synthetic.main.fragment_first.*
 import kotlinx.android.synthetic.main.fragment_first.view.*
+import org.jetbrains.anko.matchParent
+import org.jetbrains.anko.startActivity
+import org.jetbrains.anko.support.v4.find
 import org.jetbrains.anko.support.v4.startActivity
 
 
@@ -20,16 +29,21 @@ import org.jetbrains.anko.support.v4.startActivity
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
-private lateinit var listTeam: RecyclerView
-private lateinit var progressBar: ProgressBar
-private lateinit var swipeRefresh: SwipeRefreshLayout
-private lateinit var spinner: Spinner
-
 /**
  * A simple [Fragment] subclass.
  *
  */
-class FirstFragment : Fragment() {
+class FirstFragment : Fragment(), MainView {
+
+    private var matches: MutableList<Match> = mutableListOf()
+    private lateinit var presenter: MainPresenter
+    private lateinit var adapter: MatchAdapter
+    private lateinit var listMatch: RecyclerView
+    private lateinit var progressBar: ProgressBar
+    private lateinit var swipeRefresh: SwipeRefreshLayout
+    private lateinit var spinner: Spinner
+    private lateinit var leagueName: String
+
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
@@ -37,7 +51,21 @@ class FirstFragment : Fragment() {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_first, container, false)
 
-        view.item_list.setOnClickListener(clickListener)
+
+        //binding
+        listMatch = view.findViewById(R.id.rvMatches)
+
+        adapter = MatchAdapter(matches)
+        listMatch.adapter = adapter
+
+        //layout manager
+        listMatch.layoutManager = LinearLayoutManager(context)
+
+
+        val request = ApiRepository()
+        val gson = Gson()
+        presenter = MainPresenter(this, request, gson)
+        presenter.getLastMatches("4328")
 
         return view
     }
@@ -46,8 +74,27 @@ class FirstFragment : Fragment() {
     val clickListener = View.OnClickListener {view ->
 
         when (view.getId()) {
-            R.id.item_list -> startActivity<MatchDetailActivity>()
+            R.id.rvMatches -> startActivity<MatchDetailActivity>()
         }
+    }
+//
+//    override fun showLoading() {
+//        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+//    }
+//
+//    override fun hideLoading() {
+//        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+//    }
+
+//    override fun showTeamList(data: List<Team>) {
+//        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+//    }
+
+    override fun showMatchList(data: List<Match>) {
+//        swipeRefresh.isRefreshing = false
+        matches.clear()
+        matches.addAll(data)
+        adapter.notifyDataSetChanged()
     }
 
 
